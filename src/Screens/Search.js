33093204ba2739f30a7039704e362162113554b8
Blackgover <badbaby6540@gmail.com>
2023-01-09 01:21:25 +0300

    table

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import Table from "react-bootstrap/Table";
 import './style.css'
 
 export const Search = () => {
     return(
         <div className="table">
+            <div>
+                <title>Taita Taveta human rights network</title>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -14,18 +16,16 @@ export const Search = () => {
                         <th>Date Of Birth</th>
                         <th>Gender</th>
                         <th>Ethnicity</th>
+                        <th>Number Of Children</th>
+                        <th>Marital Status</th>
+                        <th>Disability</th>
+                        <th>Education Standard</th>
+                        <th>Nationality</th>
+                        <th>Residence</th>
                         <th>County</th>
                         <th>Constituency</th>
                         <th>Ward</th>
                         <th>Occupation</th>
-                        <th>Residence</th>
-                        <th>Marital Status</th>
-                        <th>Number Of Children</th>
-                        <th>Disability</th>
-                        <th>Nationality</th>
-                        <th>Education Standard</th>
-                        <th>Single Parent</th>
-                        <th>Nation of originality</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -36,18 +36,16 @@ export const Search = () => {
                         <td>12/21/1999</td>
                         <td>Male</td>
                         <td>African</td>
+                        <td>0</td>
+                        <td>None</td>
+                        <td>Single</td>
+                        <td>Masters</td>
+                        <td>Kenyan</td>
+                        <td>Kenya</td>
                         <td>Nairobi</td>
                         <td>Westlands</td>
                         <td>Kangemi</td>
                         <td>Lawyer</td>
-                        <td>Kenya</td>
-                        <td>Single</td>
-                        <td>0</td>
-                        <td>None</td>
-                        <td>Kenyan</td>
-                        <td>Masters</td>
-                        <td>No</td>
-                        <td>Kenya</td>
                     </tr>
                 </tbody>
             </table>
